18afcf99d531bfd5f5a4d302f6fd8e0c88fb2cea
Dmitry Fursevich <fursevich.dmitry@gmail.com>
2015-08-21 01:10:38 +0300

    forex script

--- forex.R
+++ forex.R
@@ -1,20 +1,31 @@
-data <- read.csv("forex/EURUSD_hour.csv", colClasses ="character")
-data[1:10,"X.OPEN."]
+data <- head(read.csv("EURUSD_hour.csv", colClasses ="character"))
+output <- data.frame(sample = NA, duplicate = NA, matches = NA)
+dfrow <- 1
+ascending = TRUE
+ascending
 for(i in 1:nrow(data)) {
-  sample <- data[i, ]
-  for(j in (i+1):nrow(data)) 
-    if(i+1 <= nrow(data)) {
-      matches <- 0
-      for(V in 1:ncol(data)) {
-        if(data[j,V] == sample[,V]) {       
-          matches <- matches + 1
-        }
-      }
-      if(matches > 3) {
-        duplicate <- data[j, ]
-        pair <- cbind(rownames(sample), rownames(duplicate), matches)
-        output[dfrow, ] <- pair
-        dfrow <- dfrow + 1
-      }
-  }
+  i
+  currentRow <- data[i, ]
+  currentRow
+#   for(j in (i+1):nrow(data)) {
+#     nextRow <- data[j,]
+#     delta = as.numeric(nextRow["X.OPEN."]) - as.numeric(currentRow["X.OPEN."])
+#     delta
+    
+#     ifdelta <- delta > 0
+#     if (xor(ascending, ifdelta)) {
+#       ascending = !ascending
+#       delta
+#       break
+#     }    
+#   }
 }
+
+
+
+# if (delta > 0 & descending) {
+#   output[dfrow, ] <- [currentRow["X.DATE."], currentRow["X.DATE."], j, delta]
+#   dfrow <- dfrow + 1
+#   
+#   break
+# } 

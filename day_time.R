41afc9ee7ebf713e77201bb7d84c1b6c8d8714cc
Dmitry Fursevich <Dzmitry_Fursevich@epam.com>
2015-08-26 18:58:46 +0300

    script

--- day_time.R
+++ day_time.R
@@ -1,8 +1,11 @@
-data <- head(read.csv("EURUSD_hour.csv", 
+data <- read.csv("EURUSD_hour.csv", 
                       colClasses =c(rep("character", 3), rep("numeric", 4)), 
-                      col.names = c("cur", "date", "time", "open", "low", "high", "close")),10)
+                      col.names = c("cur", "date", "time", "open", "low", "high", "close"))
+data$date <- as.Date(data$date, format="%Y%m%d")
 
 data$delta <- data$close - data$open
 data$maxDelta <- data$high - data$low
 
-data
+output <- aggregate(maxDelta ~ time, data, mean)
+# output[order(output$maxDelta),]
+output

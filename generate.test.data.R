cac097f493bc1afc6c48fc8585c8b8e8cf9e65ab
Dmitry Fursevich <Dzmitry_Fursevich@epam.com>
2016-03-05 19:21:51 +0300

    script

--- generate.test.data.R
+++ generate.test.data.R
@@ -1,12 +1,17 @@
 generate.test.data <- function(data, intervals) {
-#   lapply(data, function(col) {
-    lapply(intervals, function(interval) {
-      diff(data$eurjpy, interval)
+ list <- lapply(data, function(col) {
+    intervals.list <- lapply(intervals, function(interval) {
+      interval.diff <- diff(col, interval)      
+      length(interval.diff) <- length(data$eurjpy)
+      interval.diff
     })
-#   })
+   intervals.df <- do.call(cbind.data.frame, intervals.list)
+   names(intervals.df) <- intervals
+   intervals.df
+ })
+  do.call(cbind.data.frame, list)
 }
 
 generate.test.data.data <- data.frame(eurjpy = c(0,1,2,3,4,5,6,7,8,9), usdjpy = c(9,8,7,6,5,4,3,2,1,0))
-generate.test.data.intervals <- c(2, 2)
-result <- generate.test.data(generate.test.data.data, generate.test.data.intervals)
-result
+generate.test.data.intervals <- c(2, 3, 4)
+generate.test.data(generate.test.data.data, generate.test.data.intervals)

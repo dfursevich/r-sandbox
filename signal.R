2b56fd161fbc4acc90ca107bde8d449abf83da1b
Dmitry Fursevich <Dzmitry_Fursevich@epam.com>
2016-04-06 18:03:15 +0300

    script

--- signal.R
+++ signal.R
@@ -1,15 +1,17 @@
 data <- merge.files("./input/", "DAT_MT_([^_]+).+201508\\.csv")
 
-draw.currency.macd.plot(data[1:200,], 'eurjpy', TRUE)
+draw.currency.macd.plot(data, 'gbpcad', FALSE)
 
-test.intervals <- seq(5, 50, 3)
+test.intervals <- seq(3, 30, 3)
 
 test.data <- generate.test.data(data, test.intervals)
 
 signals <- generate.signals(data)
 
-test.results <- test.signals(data, signals, test.intervals, test.data, 1000)
+signals.results <- test.signals(data, signals, test.intervals, test.data, 1000)
 
 sapply(test.results, function(interval.results) {
   sum(interval.results, na.rm = TRUE)
 })
+
+aggregate(. ~ currency, data = cbind(signals, signals.results), FUN = sum)
--- test.signals.R
+++ test.signals.R
@@ -5,8 +5,7 @@ test.signals <- function(data, signals, test.intervals, test.data, lot) {
     data.value <- data[row_index, paste(signal$currency, '', sep='')]
     sapply(test.intervals, function(interval) {
       test.data.value <- test.data[row_index, paste(signal$currency, interval, sep = ".")] 
-      
-      (test.data.value - data.value) * lot * sign(signal$signal)
+      ifelse(is.null(data.value), NA, (test.data.value - data.value) * lot * sign(signal$signal))      
     })
   })
   
@@ -17,7 +16,7 @@ test.signals <- function(data, signals, test.intervals, test.data, lot) {
 
 test.signals.data <- data.frame(eurjpy = c(1,3,6,10,15,21,28,36,45,55))
 test.signals.test.data <- data.frame(eurjpy.2 = c(6,10,15,21,28,36,45,55,NA,NA), eurjpy.5 = c(21,28,36,45,55,NA,NA,NA,NA,NA))
-test.signals.signals <- data.frame(signal = c(0,1,2,3,4,5,6,7,8,9), currency=rep('eurjpy', 10))
+test.signals.signals <- data.frame(signal = c(0,NA,2,3,4,5,6,7,8,9), currency=c('eurjpy', NA, rep('eurjpy', 8)))
 test.signals.test.intervals <- c(2,5)
 test.signals.lot <- 10
 test.signals(test.signals.data, test.signals.signals, test.signals.test.intervals, test.signals.test.data, test.signals.lot)

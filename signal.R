ebfe6e7da36703275017775f3d0501ffccf6b04c
Dmitry Fursevich <Dzmitry_Fursevich@epam.com>
2016-04-08 16:26:54 +0300

    script

--- signal.R
+++ signal.R
@@ -1,6 +1,6 @@
 data <- merge.files("./input/", "DAT_MT_([^_]+).+201508\\.csv")
 
-draw.currency.macd.plot(data, 'gbpcad', FALSE)
+draw.currency.macd.plot(data, 'gbpnzd', FALSE)
 
 test.intervals <- seq(3, 30, 3)
 
@@ -10,8 +10,12 @@ signals <- generate.signals(data)
 
 signals.results <- test.signals(data, signals, test.intervals, test.data, 1000)
 
-sapply(test.results, function(interval.results) {
+sapply(signals.results, function(interval.results) {
   sum(interval.results, na.rm = TRUE)
 })
 
-aggregate(. ~ currency, data = cbind(signals, signals.results), FUN = sum)
+percentile <- round(ecdf(signals$signal)(signals$signal), digits=1)
+
+aggregate(. ~ currency + percentile, data = cbind(signals, percentile, signals.results), FUN = sum)
+
+aggregate(. ~ currency, data = cbind(signals, percentile, signals.results), FUN = function(x) {round(length(x[sign(x) == -1]) / length(x[sign(x) == 1]), 2)})

79e0c8d54f207e2e77b8d92bc8702692c17b2ce1
Dmitry Fursevich <Dzmitry_Fursevich@epam.com>
2016-05-10 15:56:23 +0300

    wasted time

--- functions/close.positions.R
+++ functions/close.positions.R
@@ -1,15 +1,57 @@
-close.positions <- function(data) {
-  list <- lapply(1:nrow(data), function(row_index) {
-    row <- data[row_index, ]
-    lapply()
+open.close.positions <- function(indicators, data, test.data, stop.loss = 0.001, take.profit = 0.001) {
+  interval.result.matrix <- sapply(seq_along(indicators), function(col_index) {
+    currency.name <- names(indicators[col_index])
+    currency.indicators <- indicators[, col_index]
+    currency.data <- data[, currency.name]
+    currency.test.data <- test.data[currency.name]
+    #     currency.open.close.positions <- function(currency.indicators, currency.data, currency.test.data, stop.loss.percent, take.profit.percent) {
+    #       close.position.results <- sapply(seq_along(currency.test.data), function(col_index) {
+    #         currency.test.data.column <- currency.test.data[, col_index]
+    #         close.position(currency.indicators, currency,data, currency.test.data.column, stop.loss.percent, take.profit.percent)
+    #       })
+    #             
+    # #       for(row_index in 1:nrow(close.position.results)) {
+    # #           row <- close.position.results[row_index, ]
+    # #           
+    # #       }
+    # #       
+    # #       
+    # #       (currency.test.data - currency.data) * lot * sign(currency.indicators)
+    # #       
+    # #       list <- lapply(1:nrow(close.position.results), function(row_index) {
+    # #         row <- close.position.results[row_index, ]
+    # #         
+    # #         for (x in row) {
+    # #           if (x == TRUE) 
+    # #         }        
+    # #       })
+    #     }    
+    #     currency.close.position(currency.indicators, currency.data, currency.test.data, stop.loss.percent, take.profit.percent)      
     
+    lapply(1:nrow(currency.indicators), function(row_index){
+      currency.indicator.value <- currency.indicators[row_index]
+      previous.currency.value <- currency.data[row_index] 
+      for (test.value in currency.test.data[row_index, ]) {        
+        
+      }
+    })
+    
+    close.position.results <- sapply(seq_along(currency.test.data), function(col_index) {
+      currency.test.data.inreval <- currency.test.data[, col_index]
+      
+      currency.test.data.inreval/currency.data > take.profit.percent
+      
+      
+      (currency.test.data.inreval - currency.data) * sign(currency.indicators)
+      
+      close.positions(currency.indicators, currency,data, currency.test.data.column, stop.loss.percent, take.profit.percent)
+    })
   })
 }
 
-close.positions.data <- data.frame(eurjpy.5 = sample.int(100, 100), eurjpy.10 = sample.int(100, 100), eurjpy.15 = sample.int(100, 100), eurjpy.20 = sample.int(100, 100))
-close.positions.results <- close.positions(close.positions.data)
-close.positions.results
-
-sapply(close.positions.data[1,], function(x) {
-  if (x )
-})
+open.close.positions.indicators <- data.frame(eurjpy = c(0,NA,1,1,1,-1,-1,-1,-1,-1), usdjpy = c(0,NA,1,1,1,-1,-1,-1,-1,-1))
+open.close.positions.data <- data.frame(eurjpy = c(1,3,6,10,15,21,28,36,45,55), usdjpy = c(1,3,6,10,15,21,28,36,45,55))
+open.close.positions.test.data <- list(eurjpy = data.frame('2' = c(6,10,15,21,28,36,45,55,NA,NA), '5' = c(21,28,36,45,55,NA,NA,NA,NA,NA), check.names=FALSE), usdjpy=data.frame('2' = c(6,10,15,21,28,36,45,55,NA,NA), '5' = c(21,28,36,45,55,NA,NA,NA,NA,NA), check.names=FALSE))
+open.close.positions.result <- open.close.positions(open.close.positions.indicators, open.close.positions.data, open.close.positions.test.data)
+open.close.positions.result
+do.call(cbind.data.frame, test.indicators.result)
--- functions/generate.test.data.R
+++ functions/generate.test.data.R
@@ -1,4 +1,4 @@
-generate.test.data <- function(data, intervals) {
+generate.test.data <- function(data, intervals, flat = TRUE) {
  list <- lapply(seq_along(data), function(col_index) {
    col <- data[, col_index]
    intervals.matrix <- sapply(intervals, function(interval) {
@@ -6,16 +6,19 @@ generate.test.data <- function(data, intervals) {
       length(interval.shift) <- length(col)
       interval.shift
     })
-   intervals.df <- data.frame(intervals.matrix)
-   intervals.names <- lapply(intervals, function(interval) {
-     paste(names(data[col_index]), interval, sep = ".")
-   })
-   names(intervals.df) <- unlist(intervals.names)
+   intervals.df <- data.frame(intervals.matrix)   
+   names(intervals.df) <- intervals
    intervals.df
  })
- do.call(cbind.data.frame, list)
+ names(list) <- names(data) 
+ 
+ if (flat) {
+   do.call(cbind.data.frame, list)
+ } else {
+   list
+ }
 }
 
 generate.test.data.data <- data.frame(eurjpy = c(1,3,6,10,15,21,28,36,45,55), usdjpy = c(10,19,27,34,40,45,49,52,54,55))
 generate.test.data.intervals <- c(2,4)
-generate.test.data(generate.test.data.data, generate.test.data.intervals)
+generate.test.data(generate.test.data.data, generate.test.data.intervals, FALSE)
